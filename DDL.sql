7c846ea5ca28badaffd7e3fdd747d0c188d728ee
cynthializbeth <cyndi_cieusagi@ciencias.unam.mx>
2022-06-09 22:48:05 -0500

    transporte y manejar

--- DDL.sql
+++ DDL.sql
@@ -1,6 +1,9 @@
 DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 
+/*-----------------------------------------------------------------------------------------
+ *-------------------------------------[ Entidades ]---------------------------------------
+ *---------------------------------------------------------------------------------------*/
 CREATE TABLE personaPiso(
     idPersona VARCHAR(10) NOT NULL UNIQUE,
     idSucursal VARCHAR(10) NOT NULL,
@@ -42,7 +45,23 @@ CREATE TABLE sucursal(
     cp INT NOT NULL
 );
 
+CREATE TABLE transporte(
+    idTransporte VARCHAR(10) NOT NULL UNIQUE,
+    marca VARCHAR(20) NOT NULL,
+    modelo VARCHAR(20) NOT NULL,
+    esMotocicleta BOOLEAN NOT NULL,
+    esBicicleta BOOLEAN NOT NULL,
+    noLicencia VARCHAR(10) NOT NULL
+);
 
+CREATE TABLE ticket();
+/*-----------------------------------------------------------------------------------------
+ *-------------------------------------[ Relaciones ]--------------------------------------
+ *---------------------------------------------------------------------------------------*/
+CREATE TABLE manejar(
+    idPersona VARCHAR(10) NOT NULL UNIQUE,
+    idTransporte VARCHAR(10) NOT NULL UNIQUE
+);
 
 /*-----------------------------------------------------------------------------------------
  *----------------------------------[ Llaves Primarias ]-----------------------------------
@@ -52,15 +71,22 @@ CREATE TABLE sucursal(
  * Llave de persona piso.
  */
  ALTER TABLE personaPiso ADD CONSTRAINT personaPiso_pkey PRIMARY KEY(idPersona);
- COMMENT ON CONSTRAINT personaPiso_pkey ON personaPiso IS 'La llave primaria del persona';
+ COMMENT ON CONSTRAINT personaPiso_pkey ON personaPiso IS 'La llave primaria de la persona';
+
 /*
  * Llave de sucursal.
  */
  ALTER TABLE sucursal ADD CONSTRAINT sucursal_pkey PRIMARY KEY(idSucursal);
- COMMENT ON CONSTRAINT sucursal_pkey ON sucursal IS 'La llave primaria del sucursal';
+ COMMENT ON CONSTRAINT sucursal_pkey ON sucursal IS 'La llave primaria de la sucursal';
+
+/*
+ * Llave de transporte.
+ */
+ ALTER TABLE transporte ADD CONSTRAINT transporte_pkey PRIMARY KEY(idTransporte);
+ COMMENT ON CONSTRAINT transporte_pkey ON transporte IS 'La llave primaria de transporte';
  
 /*----------------------------------------------------------------------------------------
- *----------------------------------[ Llaves Foraneas ]-----------------------------------
+ *-----------------------------[ Llaves Foraneas Entidades ]------------------------------
  *---------------------------------------------------------------------------------------*/
 
 /*
@@ -69,3 +95,18 @@ CREATE TABLE sucursal(
 ALTER TABLE personaPiso ADD CONSTRAINT personaPiso_fkeySucursal FOREIGN KEY(idSucursal)
 REFERENCES sucursal(idSucursal) ON UPDATE CASCADE ON DELETE CASCADE;
 COMMENT ON CONSTRAINT personaPiso_fkeySucursal ON personaPiso IS 'La llave foranea de persona piso que hace referencia a la sucursal';
+
+
+/*----------------------------------------------------------------------------------------
+ *---------------------------[ Llaves Foraneas de Relaciones ]----------------------------
+ *---------------------------------------------------------------------------------------*/
+
+/*
+ * Llaves de manejar.
+ */
+ALTER TABLE manejar ADD CONSTRAINT manejar_fkey1 FOREIGN KEY (idPersona) 
+REFERENCES personaPiso(idPersona) ON UPDATE CASCADE ON DELETE CASCADE;
+COMMENT ON CONSTRAINT manejar_fkey1 ON manejar IS 'llave foranea de la tabla manejar que hace referencia al idPersona de personaPiso';
+ALTER TABLE manejar ADD CONSTRAINT manejar_fkey2 FOREIGN KEY (idTransporte) 
+REFERENCES transporte(idTransporte) ON UPDATE CASCADE ON DELETE CASCADE;
+COMMENT ON CONSTRAINT manejar_fkey2 ON manejar IS 'llave foranea de la tabla manejar que hace referencia al idTransporte de transporte';
