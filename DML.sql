ab28ecf4d1ec47a00beae7525b9b60a37e436af4
cynthializbeth <cyndi_cieusagi@ciencias.unam.mx>
2022-06-11 13:30:16 -0500

    DML platillo

---
+++ DML.sql
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------
+ -------------------------[ Datos de Platillo ]--------------------------
+ ----------------------------------------------------------------------*/
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (1,'Taco Pastor');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (2,'Taco Costilla');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (3,'Taco Bistec');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (4,'Taco Arrachera');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (5,'Taco Suadero');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (6,'Mixiote Pollo');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (7,'Pozole');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (8,'Enchiladas Verdes');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (9,'Enchiladas Rojas');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (10,'Torta Pierna');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (11,'Torta Milanesa');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (12,'Torta Jamon');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (13,'Flauta Pollo');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (14,'Flauta Papa');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (15,'Flauta Res');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (16,'Taco longaniza');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (17,'Taco sesos');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (18,'Taco tripa');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (19,'Huevo Revuelto');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (20,'Huevo a la mexicana');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (21,'Huevo estrellado');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (22,'Huevos Divorciados');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (23,'Huevo Ranchero');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (24,'Taco Pastor');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (25,'Orden Costilla');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (26,'Orden Bistec');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (27,'Orden Arrachera');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (28,'Orden Suadero');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (29,'Agua de Jamaica');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (30,'Agua de Horchata');
+INSERT INTO platillo(idProducto,tipoPlatillo) VALUES (31,'Refresco');
