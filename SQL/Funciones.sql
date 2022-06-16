1c2045b4635f98ddaa872ad9a0f66063bbc49339
victor115 <victorvillalobos@ciencias.unam.mx>
2022-06-15 23:14:52 -0500

    funciones Actualizadas

--- SQL/Funciones.sql
+++ SQL/Funciones.sql
@@ -34,7 +34,7 @@ CREATE OR REPLACE FUNCTION subtotalTicket(idTicketP varchar)
 	$$
 	Language plpgsql;
 
---SELECT subtotalTicket('25795865')
+--LECT subtotalTicket('25795865')
              
 --------------------------------------
 CREATE OR REPLACE FUNCTION precioActualSalsa(idSalsa varchar)
@@ -51,3 +51,73 @@ CREATE OR REPLACE FUNCTION precioActualSalsa(idSalsa varchar)
 	Language plpgsql;
     
 --SELECT precioActualSalsa('1');
+	
+	
+CREATE OR REPLACE FUNCTION puntoValido() RETURNS TRIGGER
+AS 
+$$
+BEGIN
+  IF (SELECT 1 FROM persona WHERE idPersona=NEW.idPersona AND noPuntos < subtotalTicket(New.idTicket))THEN
+      RAISE EXCEPTION 'La persona que se desea agregar a la tabla manejar no es un Repartidor';
+  END IF;
+  RETURN null;
+  END;
+  $$
+LANGUAGE plpgsql;
+
+--procedimiento que aumenta el sueldo en 1500 el salario a aprtir de 2 años
+
+CREATE OR REPLACE PROCEDURE aumentarSueldo()
+	AS $$
+	BEGIN
+		UPDATE persona
+		SET salario=salario +1500 
+		WHERE antiguedad > 2;
+	END;
+	$$
+	Language plpgsql;
+--ELECT salario , antiguedad,idPersona FROM persona WHERE idPersona=6063150911 OR idPersona=2359755788;
+call aumentarSueldo();
+
+CREATE OR REPLACE FUNCTION clientenull() RETURNS TRIGGER
+ AS $$
+ DECLARE 
+ nombro varchar;
+ rfcc varchar;
+ correoo varchar;
+ telefonoo int;
+ estadoo varchar;
+ callee varchar;
+ municipioo varchar;
+ numeroo int;
+ cpp int;
+ BEGIN
+ SELECT nombreSucursal INTO nombro FROM sucursal;
+  SELECT rfc INTO rfcc FROM sucursal;
+   SELECT correo INTO correoo FROM sucursal;
+    SELECT telefono INTO telefonoo FROM sucursal;
+	 SELECT estado  INTO estadoo FROM sucursal;
+	  SELECT calle INTO callee FROM sucursal;
+	  SELECT municipio INTO municipioo FROM sucursal;
+	  SELECT numero INTO numeroo FROM sucursal;
+	  SELECT cp INTO cpp FROM sucursal;
+	 UPDATE persona 
+	 SET nombre = nombro,
+	 rfc = rfcc,
+	 correo = correoo,
+	 telefono = telefonoo,
+	 estado = estadoo,
+	 calle= callee,
+	 municipio = municipioo,
+	 numero = numeroo,
+	 cp = cpp
+	 WHERE idPersona = 0;
+	 END;
+	 $$
+	 Language plpgsql;
+ select * FROM persona WHERE idPersona= 0;
+
+CREATE OR REPLACE TRIGGER nullcliente
+BEFORE INSERT ON persona
+FOR EACH ROW
+EXECUTE PROCEDURE clientenull();
