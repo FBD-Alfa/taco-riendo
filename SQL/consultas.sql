587eab4b5e6433a1e4e406776e757217f9f9b7be
ElenaG <72757877+ElenaG89@users.noreply.github.com>
2022-06-15 19:48:27 -0700

    Update consultas.sql

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -57,5 +57,41 @@ SELECT nombre,apellidoP,apellidoM,estado,calle,municipio,numero,cp,idTicket
 FROM ticket cross join persona
 WHERE aDomicilio = True and persona.idPersona = ticket.idPersona;
 
+--Numero de tickets atendidos que compraron algun tipo de Huevos por año
+SELECT a.idProducto, a.tipoPlatillo, date_part('year', b.fecha) as anio, count(a.idProducto) as numplat
+FROM platillo a INNER JOIN precioPlatillo b ON a.idProducto=b.idProducto
+JOIN incluirPlatillo c ON c.idProducto=b.idProducto
+JOIN ticket d ON c.idTicket=d.idTicket
+WHERE a.tipoPlatillo like '%Huev%'
+GROUP BY anio, a.idProducto, a.tipoPlatillo
+ORDER BY anio, numplat DESC;
 
+-- Total de ventas de las sucursales con menores ventas
+select s.idSucursal, s.nombreSucursal, sum(round(w.precio)) as tot FROM (( SELECT  IP.idProducto, precioActualPlatillo(IP.idProducto) AS precio 
+                        FROM incluirPlatillo IP 
+                        UNION 
+                        SELECT  IP.idProducto, precioActualSalsa(IP.idProducto) AS precio 
+                        FROM incluirSalsa IP) p INNER JOIN incluirPlatillo a
+                        ON p.idProducto=a.idProducto) as w
+                        JOIN ticket t ON w.idTicket=t.idTicket
+                        JOIN sucursal s ON s.idSucursal=t.idSucursal
+                        GROUP BY s.idSucursal
+                        ORDER BY tot 
+                        LIMIT 15;
+                        
+---Platillos mas caros para todos los años
+SELECT a.idProducto, a.tipoPlatillo, max(b.precio) as mpre
+FROM platillo a INNER JOIN precioPlatillo b ON a.idProducto=b.idProducto
+JOIN incluirPlatillo c ON c.idProducto=b.idProducto
+JOIN ticket d ON c.idTicket=d.idTicket
+GROUP BY a.idProducto
+ORDER BY mpre DESC;
 
+--Platillos mas caros por años
+SELECT a.idProducto, a.tipoPlatillo, date_part('year', b.fecha) as anio, max(b.precio) as mpre
+FROM platillo a INNER JOIN precioPlatillo b ON a.idProducto=b.idProducto
+JOIN incluirPlatillo c ON c.idProducto=b.idProducto
+JOIN ticket d ON c.idTicket=d.idTicket
+--WHERE a.tipoPlatillo like 'Taco%'
+GROUP BY a.idProducto, anio
+ORDER BY mpre DESC;
